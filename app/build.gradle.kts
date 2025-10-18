115c7a105546d60ccfd063f12f797fe455e18289
Richard Gutierrez <rcgvaliente@gmail.com>
2025-10-18 09:49:55 -0500

    feat: Integrate Firebase into the project
    Adds the necessary Firebase configuration and dependencies.
    
    - Includes the `google-services.json` file to connect to the Firebase project.
    - Adds the Google Services Gradle plugin at the project level.
    - Applies the Google Services plugin in the app-level `build.gradle.kts`.
    - Adds the Firebase Bill of Materials (BoM).
    - Adds dependencies for Firestore and Firebase Auth.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -49,6 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +59,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    //Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
 }
